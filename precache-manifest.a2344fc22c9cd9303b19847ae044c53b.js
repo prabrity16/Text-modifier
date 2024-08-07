b13b9dcb91a350488fdf0b096e8b7204fa4eb72b
prabrity16 <158729422+prabrity16@users.noreply.github.com>
2024-08-07 18:17:40 +0530

    Updates

---
+++ precache-manifest.a2344fc22c9cd9303b19847ae044c53b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0f6cb15932dd8bdad6ee79ed23b109",
+    "url": "/Text-modifier/index.html"
+  },
+  {
+    "revision": "b4155853b1133bb05e23",
+    "url": "/Text-modifier/static/css/main.8c9c472a.chunk.css"
+  },
+  {
+    "revision": "8ea57846da87c05e2447",
+    "url": "/Text-modifier/static/js/2.f8a3d5ae.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Text-modifier/static/js/2.f8a3d5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/Text-modifier/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "b4155853b1133bb05e23",
+    "url": "/Text-modifier/static/js/main.c2e1d8a9.chunk.js"
+  },
+  {
+    "revision": "ece75bcd7cb92fe517ce",
+    "url": "/Text-modifier/static/js/runtime-main.9975a746.js"
+  }
+]);
